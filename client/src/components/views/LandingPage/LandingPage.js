a2bfa42e148ff7bf9d073e3598fdfb552503e151
JayyBee <binns.jaison@gmail.com>
2020-06-06 12:08:12 -0400

    image carousel works as intended

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Icon, Row, Col, Card } from "antd";
+import ImageSlider from "../../utils/ImageSlider";
+
+const { Meta } = Card;
 
 function LandingPage() {
   //create a state for product list
@@ -17,7 +21,52 @@ function LandingPage() {
       }
     });
   }, []);
-  return <></>;
+
+  const renderCards = Products.map((product, index) => {
+    return (
+      <Col lg={6} md={8} xs={24}>
+        <Card hoverable={true} cover={<ImageSlider images={product.images} />}>
+          <Meta title={product.title} description={`$${product.price}`} />
+        </Card>
+      </Col>
+    );
+  });
+
+  return (
+    <div style={{ width: "75%", margin: "3rem auto" }}>
+      <div style={{ textAlign: "center" }}>
+        <h2>
+          JayBee's Shop <Icon type="shop" />
+        </h2>
+      </div>
+
+      {/* Filter */}
+
+      {/*Search*/}
+
+      {Products.length === 0 ? (
+        <div
+          style={{
+            display: "flex",
+            height: "300px",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h2>No product available...More shall come!</h2>
+        </div>
+      ) : (
+        <div>
+          <Row gutter={[16, 16]}>{renderCards}</Row>
+        </div>
+      )}
+      <br />
+      <br />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button>Load More</button>
+      </div>
+    </div>
+  );
 }
 
 export default LandingPage;
